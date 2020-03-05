3af5b260eca21c35819da648216a1c227609482c
FranciscoCalel <36779013+FranciscoCalel@users.noreply.github.com>
2020-03-04 20:45:23 -0600

    TK

--- Proyecto1_OLC1/Proyecto1_OLC1/Token.cs
+++ Proyecto1_OLC1/Proyecto1_OLC1/Token.cs
@@ -8,5 +8,24 @@ namespace Proyecto1_OLC1
 {
     class Token
     {
+        public enum Tipo
+        {
+            ID,
+            COMENTARIO,
+            CADENA,
+            LLAVE_IZQ,
+            LLAVE_DER,
+            PARENTESIS_IZQ,
+            PARENTESIS_DER,
+            SIGNO_MAS,
+            ASTERISCO,
+            PORCENTAJE,
+            DOS_PUNTOS,
+            GUION,
+            PUNTO,
+            SIGNO_DISYUNCION,
+            SIGNO_INTERROGACION,
+            ERROR,
+        }
     }
 }
