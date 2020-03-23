3462bd9fc30f1d5cd7756f60ef4ce87fb78b8451
FranciscoCalel <36779013+FranciscoCalel@users.noreply.github.com>
2020-03-22 22:09:14 -0600

    coommit

---
+++ Proyecto1_OLC1/Proyecto1_OLC1/EstAFD2.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto1_OLC1
+{
+    class EstAFD2
+    {
+        private int nombre;
+        private List<Estado> estadosAFN;
+        private List<TransAFD2> transiciones;
+        private Boolean agregado;
+        private Boolean inicial;
+        private Boolean aceptacion;
+        public EstAFD2(int nombre)
+        {
+            this.nombre = nombre;
+            agregado = false;
+            inicial = false;
+            aceptacion = false;
+            estadosAFN = new List<Estado>();
+            transiciones = new List<TransAFD2>();
+        }
+
+        public int getNombre()
+        {
+            return nombre;
+        }
+
+        public List<Estado> getEstadosAFN()
+        {
+            return estadosAFN;
+        }
+
+        public List<TransAFD2> getListTrans()
+        {
+            return transiciones;
+        }
+
+        public Boolean isAgregado()
+        {
+            return agregado;
+        }
+
+        public Boolean isInicial()
+        {
+            return inicial;
+        }
+
+        public Boolean isAceptacion()
+        {
+            return aceptacion;
+        }
+
+        public void setNombre(int nombre)
+        {
+            this.nombre = nombre;
+        }
+
+        public void setAgregado(Boolean agregado)
+        {
+            this.agregado = agregado;
+        }
+
+        public void setInicial(Boolean inicial)
+        {
+            this.inicial = inicial;
+        }
+
+        public void setAceptacion(Boolean aceptacion)
+        {
+            this.aceptacion = aceptacion;
+        }
+
+
+    }
+}
---
+++ Proyecto1_OLC1/Proyecto1_OLC1/TransAFD2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto1_OLC1
+{
+    class TransAFD2
+    {
+
+
+        private EstAFD2 inicial;
+        private EstAFD2 final;
+        private String simbolo;
+
+        public TransAFD2(EstAFD2 inicial, EstAFD2 final, String simbolo)
+        {
+            this.inicial = inicial;
+            this.final = final;
+            this.simbolo = simbolo;
+        }
+
+        public EstAFD2 getInicial()
+        {
+            return inicial;
+        }
+
+        public EstAFD2 getFinal()
+        {
+            return final;
+        }
+
+        public String getSimbolo()
+        {
+            return simbolo;
+        }
+
+        public void setInicial(EstAFD2 inicial)
+        {
+            this.inicial = inicial;
+        }
+
+        public void setFinal(EstAFD2 final)
+        {
+            this.final = final;
+        }
+
+        public void setSimbolo(String simbolo)
+        {
+            this.simbolo = simbolo;
+        }
+
+
+    }
+}
